9e6615d5c3e9c29aa2f72e36778b998d564938a5
Chandana-L <1hk22is027@gmail.com>
2026-02-23 14:25:12 +0530

    Added initial web project files
    Added initial web project files

---
+++ vedio2/eg.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    let message = document.getElementById("message");
+    message.innerHTML = "🚀 The iPhone Pro Max is a the best! Get ready for an amazing experience! 📱✨";
+}
